9bc43ceeeb9a92266cfd77ac0a29a9874daa7cb7
kovalenkosavva27 <90443552+kovalenkosavva27@users.noreply.github.com>
2021-10-21 20:08:08 +0300

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -321,6 +321,253 @@
       </div>
     </div>
   </div>
+  <div class="main-support">
+    <div class="container">
+      <div class="col-md-12">
+        <div class="row text-center">
+          <h2 class="support-label">
+            Поддержка<br />
+            от Drupal-coder
+          </h2>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="row main-support">
+          <div class="col-md-3 col-sm-6 col-12 support-wrapper">
+            <div class="support-body">
+              <div class="support-head">
+                <div class="support-num">01.</div>
+                <div class="support-title">
+                  Постановка задачи по Email
+                </div>
+              </div>
+              <div class="support-info">
+                <div class="support-desc">
+                  Удобная и привычная <br />модель постановки задач,<br />
+                  при которой задачи <br />фиксируются и никогда<br />
+                  не теряются.
+                </div>
+                <div class="support-bg">
+                  <img
+                    src="@/assets/img/support1.svg"
+                    class="img-fluid"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-12 support-wrapper">
+            <div class="support-body">
+              <div class="support-head">
+                <div class="support-num">02.</div>
+                <div class="support-title">
+                  Система Helpdesk – отчетность, прозрачность
+                </div>
+              </div>
+              <div class="support-info">
+                <div class="support-desc">
+                  Возможность посмотреть все заявки в работе и отработанные часы
+                  в личном кабинете через браузер.
+                </div>
+                <div class="support-bg">
+                  <img
+                    src="@/assets/img/support2.svg"
+                    alt=""
+                    class="img-fluid"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-12 support-wrapper">
+            <div class="support-body">
+              <div class="support-head">
+                <div class="support-num">03.</div>
+                <div class="support-title">
+                  Расширенная техническая поддержка
+                </div>
+              </div>
+              <div class="support-info">
+                <div class="support-desc">
+                  Возможность организации расширенной техподдержки с 6:00<br />
+                  до 22:00 без выходных.
+                </div>
+                <div class="support-bg">
+                  <img
+                    src="@/assets/img/support3.svg"
+                    alt=""
+                    class="img-fluid"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-12 support-wrapper">
+            <div class="support-body">
+              <div class="support-head">
+                <div class="support-num">04.</div>
+                <div class="support-title">
+                  Персональный менеджер проекта
+                </div>
+              </div>
+              <div class="support-info">
+                <div class="support-desc">
+                  Ваш менеджер проекта всегда в курсе текущего состояния
+                  проекта<br />
+                  и в любой момент готов ответить на любые вопросы.
+                </div>
+                <div class="support-bg">
+                  <img
+                    src="@/assets/img/support4.svg"
+                    alt=""
+                    class="img-fluid"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-12 support-wrapper">
+            <div class="support-body">
+              <div class="support-head">
+                <div class="support-num">05.</div>
+                <div class="support-title">
+                  Удобные способы оплаты
+                </div>
+              </div>
+              <div class="support-info">
+                <div class="support-desc">
+                  Безналичный расчет по договору или<br />
+                  электронные деньги:<br />
+                  WebMoney, Яндекс.Деньги, Paypal.
+                </div>
+                <div class="support-bg">
+                  <img
+                    src="@/assets/img/support5.svg"
+                    alt=""
+                    class="img-fluid"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-12 support-wrapper">
+            <div class="support-body">
+              <div class="support-head">
+                <div class="support-num">06.</div>
+                <div class="support-title">
+                  Работаем с SLA и NDA
+                </div>
+              </div>
+              <div class="support-info">
+                <div class="support-desc">
+                  Работа в рамках соглашений<br />
+                  о конфиденциальности <br />и об уровне качества<br />
+                  работ.
+                </div>
+                <div class="support-bg">
+                  <img
+                    src="@/assets/img/support6.svg"
+                    alt=""
+                    class="img-fluid"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-12 support-wrapper">
+            <div class="support-body">
+              <div class="support-head">
+                <div class="support-num">07.</div>
+                <div class="support-title">
+                  Штатные специалисты
+                </div>
+              </div>
+              <div class="support-info">
+                <div class="support-desc">
+                  Надежные штатные специалисты, никаких фрилансеров.
+                </div>
+                <div class="support-bg">
+                  <img
+                    src="@/assets/img/support7.svg"
+                    alt=""
+                    class="img-fluid"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-12 support-wrapper">
+            <div class="support-body">
+              <div class="support-head">
+                <div class="support-num">08.</div>
+                <div class="support-title">
+                  Удобные каналы связи
+                </div>
+              </div>
+              <div class="support-info">
+                <div class="support-desc">
+                  Консультации по телефону, скайпу,<br />
+                  в месенджерах.
+                </div>
+                <div class="support-bg">
+                  <img
+                    src="@/assets/img/support8.svg"
+                    alt=""
+                    class="img-fluid"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="main-advantages">
+    <div class="container">
+      <div class="col-md-12 advantages">
+        <div class="row">
+          <div class="col-md-6 offset-md-6 col-12">
+            <div class="row">
+              <h2 class="advantages-label">
+                Экспертиза в Drupal,<br />
+                опыт 14 лет!
+              </h2>
+            </div>
+            <div class="row">
+              <div class="col-sm-6 col-12 adv-list">
+                <div class="adv-body">
+                  Только системный подход – контроль версий, резервирование и
+                  тестирование!
+                </div>
+              </div>
+              <div class="col-sm-6 col-12 adv-list">
+                <div class="adv-body">
+                  Только Drupal сайты, не берем на поддержку сайты на других
+                  CMS!
+                </div>
+              </div>
+              <div class="col-sm-6 col-12 adv-list">
+                <div class="adv-body">
+                  Участвуем в разработке ядра Drupal и модулей на Drupal.org,
+                  разрабатываем <a href="#">свои модули Drupal</a>
+                </div>
+              </div>
+              <div class="col-sm-6 col-12 adv-list">
+                <div class="adv-body">
+                  Поддерживаем сайты на Drupal 5, 6, 7, 8 и 9
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="adv-bg">
+      <img src="@/assets/img/laptop.png" alt="" class="img-fluid" />
+    </div>
+  </div>
   <div class="main-tarrifs">
     <div class="container" id="tariffs">
       <div class="col-md-12">
